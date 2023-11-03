6559e1a1d2f4af2a700573efc7b79744420ebeb0
nunom4chado <nunom4chado@gmail.com>
2023-11-03 16:37:49 +0000

    completes ex02

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -2,21 +2,66 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
-#include <stdlib.h> /* srand, rand */
-#include <time.h>   /* time */
+#include <cstdlib>
 
 Base::~Base() {}
 
-Base *Base::generate(void) {
-    /* initialize random seed: */
-    srand((unsigned int)time(NULL));
+/* -------------------------------------------------------------------------- */
+/*                                   Others                                   */
+/* -------------------------------------------------------------------------- */
+
+Base *generate(void) {
 
     /* generate number between 0 and 2: */
-    int n = rand() % 3;
+    switch (std::rand() % 3) {
+    case 0:
+        return new A();
+    case 1:
+        return new B();
+    case 2:
+        return new C();
+    default:
+        return NULL;
+    }
+}
 
-    if (n == 0)
-        return new A;
-    else if (n == 1)
-        return new B;
-    return new C;
+void identify(Base *p) {
+    std::cout << "--identify via pointer--" << std::endl;
+
+    if (!p) {
+        std::cout << "pointing to NULL" << std::endl;
+        return;
+    }
+
+    if (dynamic_cast<A *>(p))
+        std::cout << "Type A" << std::endl;
+    else if (dynamic_cast<B *>(p))
+        std::cout << "Type B" << std::endl;
+    else if (dynamic_cast<C *>(p))
+        std::cout << "Type C" << std::endl;
+    else
+        std::cout << "Unkown Type" << std::endl;
+}
+
+void identify(Base &p) {
+    std::cout << "--identify via reference--" << std::endl;
+    try {
+        A &a = dynamic_cast<A &>(p);
+        (void)a;
+        std::cout << "Type A" << std::endl;
+    } catch (const std::exception &e) {
+        try {
+            B &b = dynamic_cast<B &>(p);
+            (void)b;
+            std::cout << "Type B" << std::endl;
+        } catch (const std::exception &e) {
+            try {
+                C &c = dynamic_cast<C &>(p);
+                (void)c;
+                std::cout << "Type C" << std::endl;
+            } catch (const std::exception &e) {
+                std::cout << "Unkown Type" << std::endl;
+            }
+        }
+    }
 }
